afdfeb94f6018a943d9494e9118463a34e734b4e
Dillon Woollums <Dwooll94@gmail.com>
2019-01-19 22:13:59 -0600

    Made some base classes

--- src/main/java/org/usfirst/frc/team1806/robot/subsystems/LiftSubsystem.java
+++ src/main/java/org/usfirst/frc/team1806/robot/subsystems/LiftSubsystem.java
@@ -47,7 +47,7 @@ public class LiftSubsystem  implements Subsystem {
 	private LiftStates mLiftStates;
 	private LiftPosition mLiftPosition;
 	private static boolean mCubeOverride = false;
-	private static LiftSubsystem mLiftSubsystem = new LiftSubsystem();
+	private static LiftSubsystem mLiftSubsystem = new LiftSubsystem(); //only ever 1 lift
 	public LiftSubsystem() {
 		liftLead = new TalonSRX(RobotMap.liftLead);
 		liftFollow = new TalonSRX(RobotMap.liftFollow);
